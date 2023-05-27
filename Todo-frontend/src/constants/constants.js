945ede85b740a591b1f6492a1b44f15ea34f04e0
Thafsi PV <pvthafsi@gmail.com>
2023-05-28 00:21:22 +0530

    api change

--- Todo-frontend/src/constants/constants.js
+++ Todo-frontend/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const TODO_API = "http://localhost:3005/api/todo";
-export const TODO_GETBYID_API='http://localhost:3005/api/gettodobyid'
-export const TODO_DELETECOMPLETED_API='http://localhost:3005/api/deleteiscompleted'
-export const TODO_FILTERLIST_API='http://localhost:3005/api/filterlist'
+export const TODO_API = "https://todo-app-lovat-beta.vercel.app/api/todo";
+export const TODO_GETBYID_API='https://todo-app-lovat-beta.vercel.app/api/gettodobyid'
+export const TODO_DELETECOMPLETED_API='https://todo-app-lovat-beta.vercel.app/api/deleteiscompleted'
+export const TODO_FILTERLIST_API='https://todo-app-lovat-beta.vercel.app/api/filterlist'
